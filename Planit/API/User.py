9978b4c89f2db9544c59046c6178d2fc69ff37a7
Cowboy Lynk <cowboylynk@icloud.com>
2018-03-27 23:53:05 -0400

    Reset filters

--- Planit/API/User.py
+++ Planit/API/User.py
@@ -13,10 +13,10 @@ class API_User:
 		# dictionary of filters for the possible schedules
 		self._filters = dict()
 
-		self._filters['earliest_time'] = '1000'
+		self._filters['earliest_time'] = '0800'
 		self._filters['latest_time'] = None
 
-		self._filters['credit_min'] = 12
+		self._filters['credit_min'] = 1
 		self._filters['credit_max'] = 18
 
 		self._filters['forbidden_days'] = set()
