142e26d034696da483c5244ce795b68a53039c53
Daniel <d.mccrystal512@gmail.com>
2018-03-29 12:55:17 -0400

    documented API/Schedule.py

--- Planit/API/Schedule.py
+++ Planit/API/Schedule.py
@@ -7,17 +7,24 @@ class API_Schedule:
 		self._sections = set()
 		self._num_credits = 0
 
-
+	'''
+	:param new_section: API.Section object
+	:return: True if the add was successful, false if not
+	'''
 	def add_section(self, new_section):
+		# check that we are not adding duplicate sections
 		if new_section in self._sections:
 			print(str(new_section) + " already exists in schedule")
 			return False
 
+		# check that the class we are trying to add does not overlap with
+		# any sections that already exist in this schedule
 		for existing_section in self._sections:
 			if existing_section.overlaps(new_section):
 				#print("Not adding " + str(new_section) + ". Overlaps with " + str(existing_section))
 				return False
 
+		# add section to set and increment credit count
 		self._sections.add(new_section)
 		self._num_credits += new_section.get_course().get_num_credits()
 		return True
@@ -28,6 +35,10 @@ class API_Schedule:
 	def get_sections(self):
 		return list(self._sections)
 
+	'''
+	:param other_schedule: API.Schedule object
+	:return: true if the schedules are equal, false if not
+	'''
 	def equals(self, other_schedule):
 		if len(self._sections) != len(other_schedule._sections):
 			return False
@@ -38,11 +49,15 @@ class API_Schedule:
 
 		return True
 
+	'''
+	:return: a new API.Schedule object that has all the same sections
+	'''
 	def copy(self):
 		new = API_Schedule()
 		new._sections = self._sections.copy()
 		new._num_credits = self._num_credits
 		return new
+
 
 	def __str__(self):
 		out = ""
@@ -58,12 +73,20 @@ class API_Schedule:
 			out += "\n"
 		return out
 
+	'''
+	:param colors_dict: Dictionary, maps subject+course_id to a color
+	:return: a list of dictionaries, where each dictionary represents a meet time
+	of a section, with the keys:
+		title
+		start
+		end
+		color
+
+	TODO: fix this so that each dictionary is one section, with another list of time
+	block dictionaries for meet times
+	'''
 	def convert_to_dict(self, colors_dict):
-		"""
-		:param colors_dict: maps subject+course_id to a color
-		:param colors: returns colors in order
-		:return: 
-		"""
+
 		out = []
 
 		for i, section in enumerate(self._sections):
